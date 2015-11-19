6862c8caa4472ee0a335fde492254fbe88a1fcd3
kvzhao <kevinzhaio@gmail.com>
2015-11-19 14:59:27 +0800

    Change output file format.

--- clean_data.py
+++ clean_data.py
@@ -1,5 +1,5 @@
 #! /usr/bin/python
 import subprocess, sys
-cmd = 'rm *.vtk splash*.gfs t k'
+cmd = 'rm *.vtk res*.gfs *.dat t k parallel*'
 retcode = subprocess.call(cmd, shell=True)
 if retcode != 0: sys.exit(retcode)
